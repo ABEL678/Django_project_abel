55e49eac6fd11619099e95a4a00893f2e0edb508
ABEL <abelovearn@gmail.com>
2023-08-06 14:46:38 +0300

    commit make Templates homework

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
+from catalog.models import Product, Category
+
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    content = {
+        'object_list': Product.objects.all(),
+        'title': 'Наши товары'
+    }
+    return render(request, 'catalog/index.html', content)
 
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
