f5ebbfe6a7c8c3ad25c7f93686d76b5affc4c7ed
ABEL <abelovearn@gmail.com>
2023-08-28 21:59:57 +0300

    commit add forms

--- catalog/models.py
+++ catalog/models.py
@@ -1,16 +1,18 @@
 from django.db import models
-from django.utils.text import slugify
+from django.conf import settings
 
 
 class Product(models.Model):
     product_name = models.CharField(max_length=150, verbose_name='наименование')
     description = models.TextField(max_length=400, verbose_name='описание')
     image = models.ImageField(upload_to='catalog/', verbose_name='изображение', null=True, blank=True)
-    # category = models.CharField(max_length=150, verbose_name='категория')
     category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='категория', null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='цена за покупку')
     date_of_add = models.DateField(auto_now=False, auto_now_add=True, verbose_name='дата создания')
     date_last_change = models.DateField(auto_now=True, auto_now_add=False, verbose_name='дата создания')
+
+    # owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, verbose_name='владелец',
+    #                           blank=True, null=True)
 
     def __str__(self):
         return f'{self.product_name} {self.price} {self.category}'
@@ -31,3 +33,17 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+
+
+class Version(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='продукт')
+    version_number = models.CharField(max_length=100, verbose_name='номер версии')
+    version_name = models.CharField(max_length=100, verbose_name='название версии')
+    is_current_version = models.BooleanField(default=False, verbose_name='признак текущей версии')
+
+    def __str__(self):
+        return f'{self.product}, {self.version_name}, {self.version_number}'
+
+    class Meta:
+        verbose_name = "версия"
+        verbose_name_plural = "версии"
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,10 @@
+from django.forms import inlineformset_factory
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView, CreateView, UpdateView
+
 from blog.models import Blog
-from catalog.models import Product
+from catalog.models import Product, Version
+from catalog.forms import ProductForm, VersionForm
 
 from django.shortcuts import get_object_or_404, render
 from django.views import View
@@ -21,10 +26,14 @@ def index(request):
 class ProductDetailView(View):
     template_name = 'catalog/product_detail.html'
 
-
     def get(self, request, product_id):
         product = get_object_or_404(Product, pk=product_id)
         return render(request, self.template_name, {'product': product})
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('index')
 
 
 def contacts(request):
@@ -39,3 +48,44 @@ def contacts(request):
 def blog_list_view(request):
     blogs = Blog.objects.all()
     return render(request, 'blog/list.html', {'blogs': blogs})
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.owner = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            formset = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            formset = VersionFormset(instance=self.object)
+
+        context_data['formset'] = formset
+
+        return context_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        self.object = form.save()
+
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+
+        return super().form_valid(form)
